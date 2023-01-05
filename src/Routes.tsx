0e35b4d9e7774ded479d0aad446c325bc71d598e
bicco2 <jhb_0921@naver.com>
2023-01-05 19:40:05 +0900

    feat : add userPage

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,13 +2,14 @@ import { lazy } from "react";
 import GlobalLayout from "./pages/_layout";
 
 const TodoPage = lazy(() => import("./pages/TodoPage"));
+const UserPage = lazy(() => import("./pages/UserPage"));
 
 export const routes = [
   {
     path: "/",
     element: <GlobalLayout />,
     children: [
-      { path: "/", element: <TodoPage /> },
+      { path: "/auth", element: <UserPage /> },
       { path: "/todo", element: <TodoPage /> },
     ],
   },
---
+++ src/pages/UserPage/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// import { FooterWrapper, FooterItemWrapper, FooterItem } from "./styled";
+
+const UserPage: React.FC = () => {
+  return (
+    <div>
+      <div>loginPage</div>
+    </div>
+  );
+};
+
+export default UserPage;
