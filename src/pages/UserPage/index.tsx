4bef53140059c9f1d11f458be8df14bf1c5c6887
bicco2 <jhb_0921@naver.com>
2023-01-08 13:13:05 +0900

    feat : update login enter immediately

--- src/pages/UserPage/index.tsx
+++ src/pages/UserPage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Wrapper, Label, Input } from "./styled";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
@@ -6,17 +6,54 @@ import { useNavigate } from "react-router-dom";
 const UserPage: React.FC = () => {
   const [userId, setUserId] = useState("");
   const [userPw, setUserPw] = useState("");
-  const [checkId, setCheckId] = useState(false);
-  const [checkPw, setCheckPw] = useState(false);
-  const [checkTotal, setCheckTotal] = useState(true);
+  // const [checkId, setCheckId] = useState(false);
+  // const [checkPw, setCheckPw] = useState(false);
+  // const [checkTotal, setCheckTotal] = useState(true);
+  const checkId = useRef(false);
+  const checkPw = useRef(false);
+  const checkTotal = useRef(true);
 
   const navigate = useNavigate();
 
   const onIdChange = (e: any) => {
     setUserId(e.target.value);
+    var regExp =
+      /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+    console.log("이메일 유효성 검사 :: ", regExp.test(e.target.value));
+
+    if (regExp.test(e.target.value)) {
+      // setCheckId(true);
+      checkId.current = true;
+      if (checkId.current && checkPw.current) {
+        // setCheckTotal(false);
+        checkTotal.current = false;
+      } else {
+        // setCheckTotal(true);
+        checkTotal.current = true;
+      }
+    } else {
+      // setCheckTotal(true);
+      checkTotal.current = true;
+    }
   };
   const onPwChange = (e: any) => {
     setUserPw(e.target.value);
+    var regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,10}$/;
+    console.log("비밀번호 유효성 검사 :: ", regExp.test(e.target.value));
+    if (regExp.test(e.target.value)) {
+      // setCheckPw(true);
+      checkPw.current = true;
+      if (checkId.current && checkPw.current) {
+        // setCheckTotal(false);
+        checkTotal.current = false;
+      } else {
+        // setCheckTotal(true);
+        checkTotal.current = true;
+      }
+    } else {
+      // setCheckTotal(true);
+      checkTotal.current = true;
+    }
   };
 
   const checkEmail = (e: any) => {
@@ -25,14 +62,18 @@ const UserPage: React.FC = () => {
     console.log("이메일 유효성 검사 :: ", regExp.test(e.target.value));
 
     if (regExp.test(e.target.value)) {
-      setCheckId(true);
+      // setCheckId(true);
+      checkId.current = true;
       if (checkId && checkPw) {
-        setCheckTotal(false);
+        // setCheckTotal(false);
+        checkTotal.current = false;
       } else {
-        setCheckTotal(true);
+        // setCheckTotal(true);
+        checkTotal.current = true;
       }
     } else {
-      setCheckTotal(true);
+      // setCheckTotal(true);
+      checkTotal.current = true;
     }
   };
 
@@ -40,14 +81,18 @@ const UserPage: React.FC = () => {
     var regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,10}$/;
     console.log("비밀번호 유효성 검사 :: ", regExp.test(e.target.value));
     if (regExp.test(e.target.value)) {
-      setCheckPw(true);
+      // setCheckPw(true);
+      checkPw.current = true;
       if (checkId && checkPw) {
-        setCheckTotal(false);
+        // setCheckTotal(false);
+        checkTotal.current = false;
       } else {
-        setCheckTotal(true);
+        // setCheckTotal(true);
+        checkTotal.current = true;
       }
     } else {
-      setCheckTotal(true);
+      // setCheckTotal(true);
+      checkTotal.current = true;
     }
   };
 
@@ -72,22 +117,18 @@ const UserPage: React.FC = () => {
     <Wrapper>
       <Label>Login</Label>
       ID
-      <Input
-        autoFocus
-        onChange={onIdChange}
-        onBlur={checkEmail}
-        value={userId}
-        placeholder="ID"
-      />
+      <Input autoFocus onChange={onIdChange} value={userId} placeholder="ID" />
       PW
       <Input
         onChange={onPwChange}
-        onBlur={checkPassword}
         value={userPw}
         placeholder="Passward"
         type="password"
       />
-      <button disabled={checkTotal ? true : false} onClick={handleLogin}>
+      <button
+        disabled={checkTotal.current ? true : false}
+        onClick={handleLogin}
+      >
         login
       </button>
     </Wrapper>
